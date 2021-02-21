b832ae1d46a911e2c7801a56a41ba427b9d2d3f3
Davide Radaelli <daviderady@gmail.com>
2021-02-21 13:23:23 -0700

    Good progress

--- skilltree-web/src/main.rs
+++ skilltree-web/src/main.rs
@@ -3,25 +3,96 @@
 #[macro_use]
 extern crate rocket;
 
-mod api;
-mod pages;
-
+use rocket::response::status;
+use rocket::Route;
+use rocket::State;
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
+use serde::Deserialize;
+use serde::Serialize;
+use sled_extensions::bincode::Tree;
+use sled_extensions::DbExt;
 
-#[catch(404)]
-fn not_found() -> Template {
-    let context = ();
-    Template::render("error", &context)
+#[derive(Deserialize, Serialize, Clone, Debug)]
+struct User {
+    name: String,
+    skills: Vec<usize>,
 }
 
-fn ignite() -> rocket::Rocket {
-    rocket::ignite()
-        .attach(Template::fairing())
-        .mount("/static", StaticFiles::from("static"))
-        .mount("/", pages::routes())
-        .mount("/api", api::routes())
-        .register(catchers![not_found])
+struct Database {
+    users: Tree<User>,
+}
+
+#[get("/")]
+fn index(db: State<Database>) -> Template {
+    match db.users.get("admin2").expect("DB is up") {
+        Some(v) => (),
+        None => {
+            let admin = User {
+                name: "admin2".to_string(),
+                skills: vec![0],
+            };
+            println!("inserting admin2");
+            db.users.insert("admin2", admin).expect("Inserting admin");
+            ()
+        }
+    }
+    let len = db.users.len();
+    let users: Vec<String> = db
+        .users
+        .iter()
+        .take(len - 1)
+        .map(|user| user.expect("not user").1.name)
+        .collect();
+    #[derive(Serialize)]
+    struct Context {
+        users: Vec<String>,
+    };
+
+    let context = Context { users };
+    Template::render("index", &context)
+}
+
+#[get("/admin")]
+fn admin(db: State<Database>) -> Template {
+    todo!()
+}
+
+#[get("/<name>")]
+fn user(db: State<Database>, name: String) -> Template {
+    todo!()
+    /*
+    match db.users.get("front-roll").unwrap() {
+        Some(v) => (),
+        None => {
+            db.values.insert("front-roll", 0 as usize).unwrap();
+            ()
+        }
+    }
+    let value = db.values.get("front-roll").unwrap().unwrap();
+    #[derive(Serialize)]
+    struct Context {
+        title: String,
+        value: usize,
+    };
+
+    let context = Context {
+        title: "T".to_string(),
+        value,
+    };
+
+    Template::render("index", &context)
+        */
+}
+
+#[post("/front-roll/<value>")]
+fn update(db: State<Database>, value: usize) -> status::Accepted<String> {
+    todo!()
+    /*
+    println!("{} saved to the database", &value);
+    db.users.insert("front-roll", value).unwrap();
+    status::Accepted(Some(format!("value: {}", &value)))
+        */
 }
 
 fn main() {
@@ -29,5 +100,16 @@ fn main() {
         .path("./database")
         .open()
         .expect("Failed to open sled DB");
-    ignite().launch();
+
+    rocket::ignite()
+        .attach(Template::fairing())
+        .manage(Database {
+            users: db
+                .open_bincode_tree("users")
+                .expect("failed to open user tree"),
+        })
+        .mount("/static", StaticFiles::from("static"))
+        .mount("/", routes![index, user])
+        .mount("/api", routes![update])
+        .launch();
 }
