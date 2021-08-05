05761a06954ea3190637e6fe5017f740c493c31e
Davide Radaelli <daviderady@gmail.com>
2021-08-04 22:10:42 -0700

    Change minsize again

--- static/user.js
+++ static/user.js
@@ -9,7 +9,7 @@ const elem = document.getElementsByTagName("svg")[0];
 const panzoom = Panzoom(elem, {
     excludeClass: 'skill',
     maxScale: 10,
-    minScale: 1.5,
+    minScale: 0.35,
     step: 0.15,
 });
 const parent = elem.parentElement;
